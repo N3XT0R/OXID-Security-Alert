dbc286553c9912f24d24955021636189264f8e66
N3XT0R <info@php-dev.info>
2016-06-30 22:14:09 +0200

    implemented SQL-Injection Validator.

--- ib_SecurityAlert/core/validator/ib_SecurityAlert_SQLI_Validator.php
+++ ib_SecurityAlert/core/validator/ib_SecurityAlert_SQLI_Validator.php
@@ -7,6 +7,18 @@
  * Time: 20:36
  */
 class ib_SecurityAlert_SQLI_Validator extends ib_SecurityAlert_AbstractValidator{
-    
 
+    protected $_aMatchRules = [
+        //error-based:
+        ' order by \d+',
+        '\+order\+by\+\d+',
+        '%20order%20by%20\d+',
+        'unhex\(hex\((.*)\)',
+        //Blind:
+        ' and \d+=\d+',
+        '\+and\+\d+=\d+',
+        //time-based:
+        'BENCHMARK\(\d+,ENCODE\((.*)\)\)',
+    ];
+    
 }
